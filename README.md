a168426310dd508a53f1a11af77266c2d30fdd35
Honza Pokorny <honza@pokorny.ca>
2021-09-25 15:15:56 -0300

    Make it a project

---
+++ README.md
@@ -0,0 +1,20 @@
+extract-subtitle
+================
+
+Many video files come with embedded subtitles, and many video players can't
+access those.  This tiny utility extracts the subtitle from the video file into
+a separate subtitle file which is much more widely supported.
+
+## Dependencies
+
+Golang, and ffmpeg
+
+## Install
+
+```
+$ go get github.com/honza/extract-subtitle
+```
+
+## License
+
+GPLv3
--- pkg/subtitle/subtitle.go
+++ pkg/subtitle/subtitle.go
@@ -1,3 +1,19 @@
+// extract-subtitle
+// Copyright (C) 2021  Honza Pokorny <honza@pokorny.ca>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 package subtitle
 
 import (
@@ -41,7 +57,6 @@ func getSubtitleStreamNumber(ffmpegBin string, filename string, language string)
 	for _, stream := range streams {
 		if strings.Contains(stream, "Subtitle") {
 			if strings.Contains(stream, language) {
-				fmt.Println(stream)
 				m := p.FindStringSubmatch(stream)
 				if len(m) != 0 {
 					return m[1], nil
